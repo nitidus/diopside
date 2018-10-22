5346a1c080ca52eaecbc596866c2cb19c3a87b01
Ali Alizade Haqiqi <alializade@peedes.com>
2018-10-22 15:26:20 +0330

    fix some issues.

--- pipes/gem/gem-init.js
+++ pipes/gem/gem-init.js
@@ -9,7 +9,7 @@ var today = new Date(),
   		code: "000-000",
   		confirmed: true
   	},
-  	profile: "<svg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 504 504' style='enable-background:new 0 0 504 504; background-color: #040404;' xml:space='preserve'><style type='text/css'> .st0{font-family:'WorkSans-Medium';} .st1{font-size:384.8238px; fill: #ffffff;}%0A</style><text transform='matrix(1 0 0 1 112.6943 374.6675)' class='st0 st1'>G</text></svg>",
+  	profile: "<svg id=\"Layer_1\" data-name=\"Layer 1\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 68.6 46.19\" style=\"enable-background:new 0 0 68.6 46.19; background-color: #040404;\"><defs><style>.cls-1{fill:#fff;}</style></defs><title>1</title><path class=\"cls-1\" d=\"M56.71,0H11.89L0,11.89l34.3,34.3,34.3-34.3Zm-51,11.89L13.55,4H30L15.31,18.71l19,19,19-19L42.39,7.79l-9.5,9.5,2.83,2.83,6.67-6.67,5.25,5.26L34.3,32.05,21,18.71,35.67,4H55.06L63,11.89,34.3,40.54Z\"/></svg>",
     isGem: true,
   	name: "GEM FOLLOW"
     },
