6e76d3748e1123b5f1dc8b10b4e2c024e9aba8f5
Ali Alizade Haqiqi <alializade@peedes.com>
2019-02-14 01:04:53 +0330

    some issues fixed.

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ var cookieParser = require('cookie-parser')
 var session = require('express-session')
 var MongoStore = require('connect-mongo')(session)
 var bodyParser = require('body-parser')
+var http = require('http')
 
 var app = express()
 
 var connectionUrl = 'mongodb://localhost:27017/gem-follow'
 var GEM_KEY = "$2a$08$8kGpKAcmDOsZtz5la65ywuGmpbNEaYeNBv.eNfQ/2jd9OzIRq34u."
 
-const _PORT = process.env.PORT || 3000;
+var httpServer = http.createServer(app);
+
+const _PORT = process.env.PORT || 8080;
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -44,6 +47,6 @@ var controllers = require('./pipes/controllers')(app, _PORT);
 
 var processor = require('./pipes/processor')(app, _PORT);
 
-app.listen(_PORT, function(){
+httpServer.listen(_PORT, function(){
   console.log(`Gem Follow is running on port ${_PORT}.`);
 })
