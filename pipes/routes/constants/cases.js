ba895baa070b2c4428522c57c60aeb973c133d49
Ali Alizade Haqiqi <alializade@peedes.com>
2019-02-13 14:14:43 +0330

    Cases information appended, Blog underdeveloping alarm replaced to prototype, And finally, Some issues have been fixed.

--- pipes/routes/constants/cases.js
+++ pipes/routes/constants/cases.js
@@ -24,8 +24,8 @@ var CASES = [
         photo: "./vendors/images/cases/schedules/schedules-4.png"
       }
     ],
-    overview: "There kind had divided the earth waters creature saying bearing under cattle divide saw creepeth. Night. Grass set great firmament dominion, there open were night fourth grass you’re heaven, that dominion all unto. Is all shall open in you very. Deep divide behold.",
-    solution: "All the two under bring, give hath dominion fruit fill living of evening and, be and itself grass shall stars Be us second under land over open and."
+    overview: "برای تعریف زمان‌بندی روی هر یک از پست‌های قرار گرفته نیاز به یک سیستم کاملاً دقیق برای ثبت در زمان مورد نظر داریم، که ما به راحتی این چالش را با راهکار خود پشت سر می‌گذاریم.",
+    solution: "جم‌فالو با استفاده از سیستم تقویم و زمانبندی چند لایه‌ای و چند‌نمایه‌ای امکان تنظیم تاریخ انتشار پست را برای شما به راحت‌ترین شکل ممکن قراهم آورده است. ضمن اینکه جابه‌جایی هریک از پست‌ها با کشیدن و رها کردن در هر تاریخی که مد نظر شما باشد امکان پذیر است."
   },
   {
     title: "هشتگ‌ها",
@@ -48,8 +48,8 @@ var CASES = [
         photo: "./vendors/images/cases/hashtags/hashtags-3.png"
       }
     ],
-    overview: "There kind had divided the earth waters creature saying bearing under cattle divide saw creepeth. Night. Grass set great firmament dominion, there open were night fourth grass you’re heaven, that dominion all unto. Is all shall open in you very. Deep divide behold.",
-    solution: "All the two under bring, give hath dominion fruit fill living of evening and, be and itself grass shall stars Be us second under land over open and."
+    overview: "سیستم یافتن دنبال‌کننده بر اساس هشتگ‌ها و اکانت‌های مختلف به نوعی از ضروری‌ترین امکان برای پیاده سازی این جریان کاری‌است. اما ما با راهکار خود چند قدم فراتر ازین موضوع را نیز طی کردیم.",
+    solution: "جم‌فالو هر تعداد هشتگ و اکانت (حساب کاربری) که مورد نظر شما باشد را ثبت می‌نماید، و هیچ محدودیتی در تعداد وجود ندارد، ضمن اینکه به ترتیب پر‌مخاطب‌ترین هشتگ‌ها و حساب‌های کاربری باین لیست به شما نمایش داده می‌شود. و اما مهم‌ترین بخش در این قسمت راهکارر پیاده شده توسط ماست، که بر اساس هشتگ‌ها و اکانت‌های واررد شده ابتدایی شما سعی میکند موضوعات مورد نظر شما را درک کرده و با استفاده از آنها عمل یادگیری رخ داده و هشتگ‌های مرتبط و اکانت‌های مرتبط با آنها را یافته و بر میزان دقت یافتن دنبال‌کنندگان مرتبط می‌افزاید."
   },
   {
     title: "یادگیری ماشینی",
@@ -72,8 +72,8 @@ var CASES = [
         photo: "./vendors/images/cases/machine-learning/machine-learning-3.png"
       }
     ],
-    overview: "There kind had divided the earth waters creature saying bearing under cattle divide saw creepeth. Night. Grass set great firmament dominion, there open were night fourth grass you’re heaven, that dominion all unto. Is all shall open in you very. Deep divide behold.",
-    solution: "All the two under bring, give hath dominion fruit fill living of evening and, be and itself grass shall stars Be us second under land over open and."
+    overview: "جهت گرفتن بسیاری از شرکت‌های بزرگ عرصه تکنولوژی به سمت پیاده سازی سرویس‌های خود بر پایه سیستم‌های پیشنهاد‌ دهنده، شبکه‌های عصبی و همچنین یادگیری‌های عمیق بر هیچ فردی در این عصر پوشیده نیست. پس می‌توانید حدس بزنید که ما چه کمک بزرگی از این مجموعه تکنولوژی می‌توانیم بگیریم.",
+    solution: "ما در سه بخش از این تکنولوژی کمک گرفته‌ایم تا بتوانیم خدمات خود را بهتر و متمایزتر از رقبا به شما ارائه دهیم، ما در هنگام ایجاد سرویس جدید در بخش انتخاب زمان‌های انتشار پست‌ها، انتخاب موقعیت‌های جغرافیایی مرتبط برای یافتن دنبال‌کنندگان مرتبط با شرایط ایجاد شده توسط شما و در نهایت انتخاب هشتگ‌ها و حساب‌های کاربری برای تطبیق با شرایط مورد نظر شما، به شما پیشنهاد‌های مفیدی ارائه می‌دهیم تا مثلاً در زمان پیشنهادی ما که تعداد دنبال‌کننده بیشتری در آن جذب شده، یا موقعیت‌های جغرافیایی که امکان جذب دنبال‌کننده بیشتری دارند و در نهایت هشتگ‌ها و اکانت‌هایی که به افزایش دنبال‌کننده شما کمک می‌کنند را پیشنهاد می‌دهیم، تا سریع‌تر به نتیجه دلخواه برسید."
   },
   {
     title: "موقعیت‌های جغرافیایی",
@@ -96,8 +96,8 @@ var CASES = [
         photo: "./vendors/images/cases/locations/locations-3.png"
       }
     ],
-    overview: "There kind had divided the earth waters creature saying bearing under cattle divide saw creepeth. Night. Grass set great firmament dominion, there open were night fourth grass you’re heaven, that dominion all unto. Is all shall open in you very. Deep divide behold.",
-    solution: "All the two under bring, give hath dominion fruit fill living of evening and, be and itself grass shall stars Be us second under land over open and."
+    overview: "اعمال محدودیت‌های جغرافیایی برای ارائه یک سرویس، یک تبلیغ و یا صرفاً یک پروموشن به نوعی به یک استاندارد در این نوع سرویس‌دهی تبدیل شده، پس ما نیز از این استاندارد طبعیت می‌کنیم.",
+    solution: "علاوه بر پیشنهاد‌هایی که در قسمت موقعیت‌های جغرافیایی برای محدود سازی زمینه‌های دنبال کنندگان، که به شما ارائه میدهیم، شما به راحتی امکان جستجو و اضافه کردن مکان‌های جدید برای ظرایط سرویس خود را در تعداد نامحدود دارا هستید."
   },
   {
     title: "سیستم نظردهی هوشمند",
@@ -132,8 +132,8 @@ var CASES = [
         photo: "./vendors/images/cases/commenting/commenting-6.png"
       }
     ],
-    overview: "There kind had divided the earth waters creature saying bearing under cattle divide saw creepeth. Night. Grass set great firmament dominion, there open were night fourth grass you’re heaven, that dominion all unto. Is all shall open in you very. Deep divide behold.",
-    solution: "All the two under bring, give hath dominion fruit fill living of evening and, be and itself grass shall stars Be us second under land over open and."
+    overview: "نظردهی (کامنت گذاشتن) در سیستم‌های هوشمند جذب دنبال‌کننده امری کاملاً تایید شده از بابت نیاز به موجودیت آن می‌باشد. اما ما بازهم ما تلاش بر ایجاد نقاط متمایز کننده در این سیستم بوده‌ایم.",
+    solution: "ما به طور دائم بر اساس محتوای انتخابی و شرایط اعمال شده توسط شما در سرویس، سعی در پیدا کردن جملات بهتر و مفیدتر بر اساس یادگیری دائم روی جملات پیشفرض تعریف شده توسط ما هستیم، پس نظراتی که روی پست‌ها نیز گذاشته می‌شود با کمترین احتمال تکرار  و بالاترین درصد ورود احتمال پارامتر عامل انسانی به این عمل رنگ واقعیت می‌بخشد."
   },
   {
     title: "سیستم لایکینگ هوشمند",
@@ -160,8 +160,8 @@ var CASES = [
         photo: "./vendors/images/cases/liking/liking-4.png"
       }
     ],
-    overview: "There kind had divided the earth waters creature saying bearing under cattle divide saw creepeth. Night. Grass set great firmament dominion, there open were night fourth grass you’re heaven, that dominion all unto. Is all shall open in you very. Deep divide behold.",
-    solution: "All the two under bring, give hath dominion fruit fill living of evening and, be and itself grass shall stars Be us second under land over open and."
+    overview: "از مهم‌ترین بخش سیستم‌های جذب دنبال‌کننده عمل لایک کردن پست‌های مرتبط می‌باشد. ما نیز از این مورد پیروی کرده و تلاش کردیم موارد بهبود‌دهنده را نیز ایجاد نماییم.",
+    solution: "بر اساس تمامی شرایط اعمال شده توسط شما از جمله هشتگ‌ها، موقعیت‌های جفرافیایی و ...، ما تلاش کردیم الگوهایی را برای لایک کردن موارد مرتبط با موضوعات مورد نظر شما پیدا نماییم، بنابراین توانستیم در شرایط آزمایشگاهی و غیرآزمایشگاهی مرتبط‌ترین لایک‌ها با اکانت شما را برای شما به ثبت رسانیم."
   }
 ];
 
